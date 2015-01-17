06e521a8fba8a90717c9d692fced47248f50e0b6
IanChiu <mail03394538@gmail.com>
2015-01-17 09:46:06 +0800

    catch me!! you fool!!

--- main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var content = $('.content');
-	
-             $(this).text('Show Detail');
-            content.removeClass('running').addClass('reverse');
-        }).trigger('click');
-    });
-});
