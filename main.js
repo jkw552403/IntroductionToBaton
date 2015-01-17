e6eae59b573eda52b21835d400cc99b61d02af20
IanChiu <mail03394538@gmail.com>
2015-01-17 09:40:48 +0800

    Finalize my work

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
     var content = $('.content');
 	
-    $('#trigger').live('click', function() {
-        $(this).toggle(function() {
-            $(this).text('Hide Detail');
-            content.removeClass('reverse').addClass('running');
-        }, function() {
              $(this).text('Show Detail');
             content.removeClass('running').addClass('reverse');
         }).trigger('click');
